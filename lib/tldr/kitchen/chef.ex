3fcd67d97941f1532abb98936c7d8b6d28158e75
Nathan Willson <willson.b.nathan@gmail.com>
2025-12-05 10:03:31 +0900

    simplify

--- lib/tldr/kitchen/chef.ex
+++ lib/tldr/kitchen/chef.ex
@@ -4,28 +4,28 @@ defmodule Tldr.Kitchen.Chef do
   alias Tldr.Kitchen.Recipe
   alias Tldr.Kitchen.Step
 
+  @doc """
+  Cooks a recipe by executing each step in order.
+  """
   def cook(%Recipe{steps: steps}) do
-    steps
-    |> Step.hydrate()
-    |> cook()
+    cook(steps)
   end
 
   def cook(steps, input \\ nil)
 
-  # step through each step,
-  # with error handling or report at
-  # each step
   def cook([%Step{} = step | steps], input) do
     with {:ok, output} <- cook(step, input) do
       cook(steps, output)
-    else
-      {:error, reason} -> {:error, {:step_failed, step, reason}}
     end
   end
 
   def cook(%Step{} = step, input) do
+    step = Step.hydrate(step)
+
     with {:ok, module} <- actor_module(step) do
       module.execute(step, input)
+    else
+      {:error, reason} -> {:error, {:step_failed, step, reason}}
     end
   end
 
